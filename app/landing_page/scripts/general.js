19e1746a8920b6bbcebc5ff2f4e7c55bd7880844
Diego Juan Isaquiel <diego.mizael@facens.br>
2022-11-14 20:34:36 -0300

    accesses: finalizado

--- app/landing_page/scripts/general.js
+++ app/landing_page/scripts/general.js
@@ -38,3 +38,22 @@
  /**
   * End
   */
+
+ /**
+ * Smooth transition when clicked on anchor
+ */
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+});
+
+/**
+ * End
+ */
+
