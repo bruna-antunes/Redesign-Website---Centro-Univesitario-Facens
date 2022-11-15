4c78f45079998f46f06f1a5edbd768f5eaa4af16
Diego Juan Isaquiel <diego.mizael@facens.br>
2022-11-15 11:20:30 -0300

    fix: corrigind alguns detalhes no layout

--- app/portal_facens/pages/main/home/script.js
+++ app/portal_facens/pages/main/home/script.js
@@ -5,10 +5,11 @@ function openSub() {
     document.querySelector('#sub-title').style.display = 'block';
     btn.style.background = '#0099F2';
     btn.style.color = 'white';
+    btn.style.borderRight = '8px solid #005C9E';
   } else {
     document.querySelector('#sub-title').style.display = 'none';
     btn.style.background = 'white';
     btn.style.color = '#005C9E';
-
+    btn.style.borderRight = '';
   }
 }
