08fab91663e9f7209e4db77976d0d210a57a78e0
bruna-antunes <118317410+bruna-antunes@users.noreply.github.com>
2022-11-19 15:47:43 -0300

    att

--- app/portal_facens/pages/main/home/script.js
+++
@@ -1,9 +0,0 @@
-
-const listaMeses = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
-
-const dataAtual = new Date();
-let mesAtual = listaMeses[dataAtual.getMonth()];
-let anoAtual = dataAtual.getFullYear();
-
-document.getElementById("currentMonthLabel").innerHTML = mesAtual;
-document.getElementById("currentYearLabel").innerHTML = anoAtual;
